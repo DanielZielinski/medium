97c95a17d91df092488ed75a8fc4d9f55b6e232e
DanielZielinski <dzielinski@9bits.pl>
2023-03-08 20:25:41 +0100

    cr

--- src/main/java/com/daniel/zielinski/medium/tracedapp/codereview/account/AccountService.java
+++ src/main/java/com/daniel/zielinski/medium/tracedapp/codereview/account/AccountService.java
@@ -1,7 +1,6 @@
 package com.daniel.zielinski.medium.tracedapp.codereview.account;
 
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
